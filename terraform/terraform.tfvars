fdb7cebe6383ea1c8e33611897652c71683d92b1
lakunzy <lakunzy7@gmail.com>
2025-10-26 14:11:07 +0100

    ansible-mini-finance

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,8 @@
+subscription_id = "your-subscription-id"
+tenant_id       = "your-tenant-id"
+location = "eastus"
+resource_group_name = "rg-mini-finance"
+admin_username = "azureuser"
+vm_size = "Standard_B1s"
+ssh_public_key = "~/.ssh/id_ed25519.pub"
+
---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username for Linux VMs"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "Path to your SSH public key"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "VM size"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
